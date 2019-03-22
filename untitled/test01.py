3b2f21acc77e51b86af269dee233cfbcc9db79a4
1286211699 <1286211699@qq.com>
2019-03-22 13:35:41 +0800

    test

--- untitled/test01.py
+++ untitled/test01.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2019/3/21 20:12
+# @Time    : 2019/3/22 13:35
 # @Author  : for 
 # @File    : test01.py
 # @Software: PyCharm
-import socket
-
-s=socket.socket()
-
-host=socket.gethostname()
-port=3456
-s.connect((host,port))
-
-cmd=input(">>>")
-s.sendall(cmd.encode())
-data=s.recv(1024)
-print(data.decode())
-
-s.close()
